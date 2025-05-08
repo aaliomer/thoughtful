2e0ae98115d6fa1215dbfac1ed822af17cdee2ce
jay jay <jayjay@jays-MacBook-Air.local>
2025-05-08 16:57:17 -0400

    inteview question

---
+++ interview.ts
@@ -0,0 +1,21 @@
+type PackageCategory = "STANDARD" | "SPECIAL" | "REJECTED"
+
+
+function sort(width: number, height: number, length: number, mass: number): PackageCategory {
+    const maxDimension = Math.max(width, height, length)
+    const volume = width * height * length
+    
+    const isOverweight = mass >=20
+    const isBulky = maxDimension >= 150 || volume >= 1_000_000
+
+    if (isOverweight && isBulky) {
+        return "REJECTED"
+    }
+    
+    //isOverweight || isBulky is works but it is dependent on the statment
+    if (isOverweight != isBulky) { // one is true, one is false (XOR)
+        return "SPECIAL"
+    }
+    
+    return "STANDARD"
+}
